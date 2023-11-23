da54e03a6a642032df00d992d5b8e3fb1078499e
Pavel Sh <p.sh07@ya.ru>
2023-11-23 22:03:25 +0300

    fixes

--- cpp-search-server/main.cpp
+++ cpp-search-server/main.cpp
@@ -1,8 +1,9 @@
 #include <algorithm>
-#include <iostream>
-#include <set>
-#include <map>
 #include <cmath>
+#include <iostream>
+#include <numeric>
+#include <map>
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -49,6 +50,13 @@ struct Document {
     double relevance;
 };
 
+struct QueryWord {
+    QueryWord(string word, bool is_minus = false)
+    : str(word), is_minus(is_minus) {}
+    string str;
+    bool is_minus;
+};
+
 class SearchServer {
 public:
     void SetStopWords(const string& text) {
@@ -58,21 +66,21 @@ public:
     }
 
     void AddDocument(int document_id, const string& document) {
-        int total_word_count = 0;
-        for(const auto& word : SplitIntoWordsNoStop(document)) {
-            //build the inverse index for occurence
-            ++word_index_[word][document_id];
-            ++total_word_count;
+        ++no_of_docs;
+        const auto doc_words = SplitIntoWordsNoStop(document);
+        int total_words_in_doc = accumulate(doc_words.begin(), doc_words.end(), 0, [](const auto prev, const auto word_count) {
+            return prev + word_count.second;
+        }); //build the inverse index for occurence
+        for(const auto& [word, count] : doc_words) {
+            word_index_[word][document_id] = ComputeTf(count, total_words_in_doc);
         }
-        //upd total word count for the doc (for tf)
-        doc_word_data_.push_back(total_word_count);
     }
     
     //v 2. excluding minus words
     vector<Document> FindTopDocuments(const string& raw_query) const {
         set<string> minus_words;
-        const set<string> query_words = ParseQuery(raw_query, minus_words);
-        auto matched_documents = FindAllDocumentsNoMinus(query_words, minus_words);
+        const vector<QueryWord> query_words = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocumentsNoMinus(query_words);
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
@@ -86,46 +94,43 @@ public:
 
 private:
     // doc count = doc_word_data_.size();
-    vector<int> doc_word_data_;
-    //map <word -> map<docId, count> >
-    map<string, map<int, int>> word_index_;
+    int no_of_docs = 0;
+    //map <word -> map<docId, TF> >
+    map<string, map<int, double>> word_index_;
     set<string> stop_words_;
     
     //Inverse Document Frequency (per word per all docs)
-    double get_idf(const string& word) const {
-        if(word_index_.count(word) == 0)
+    double GetIdf(const string& word) const {
+        if(word_index_.count(word) == 0) {
             return 0;
-        //log(totalDocs/number of docs containing the word)
-        return log(1.0 * get_total_doc_count()/get_word_in_docs_count(word));
+        }//log(totalDocs/number of docs containing the word)
+        return log(1.0 * GetTotalDocCount()/GetWordInDocsCount(word));
     }
     
-    //Term Frequency (per word per document)
-    double get_tf(int doc_id, int count) const {
-        if(doc_word_data_.size() <= doc_id) {
-            throw(std::out_of_range("the doc_id is outside of range stored in SearchServer"));
-        } //count in doc / total words in doc :
-        return 1.0*count/double(doc_word_data_[doc_id]);
+    //Term Frequency: word occurences / total words in doc
+    double ComputeTf(int term_count, int total_words) const {
+        return 1.0*term_count/total_words;
     }
     
     //return total number of stored documents
-    int get_total_doc_count() const {
-        return int(doc_word_data_.size());
+    int GetTotalDocCount() const {
+        return no_of_docs;
     }
     
     //return number of docs containing word
-    int get_word_in_docs_count(const string& word) const {
+    int GetWordInDocsCount(const string& word) const {
         if(word_index_.count(word) == 0) {
             return 0;
         }
-        return int(word_index_.at(word).size());
+        return static_cast<int>(word_index_.at(word).size());
     }
     
     //return list of docs containing at least one minus word
-    set<int> get_minus_docs(const set<string>& minus_words) const {
+    set<int> GetMinusDocs(const vector<QueryWord>& query_words) const {
         set<int> minus_list;
-        for(const auto& word : minus_words) {
-            if(word_index_.count(word) != 0) {
-                for(const auto [docId, count] : word_index_.at(word))
+        for(const auto& qword : query_words) {
+            if(qword.is_minus && word_index_.count(qword.str) != 0) {
+                for(const auto [docId, count] : word_index_.at(qword.str))
                 minus_list.insert(docId);
             }
         }
@@ -134,47 +139,46 @@ private:
     
     //upd for minus words
     bool IsStopWord(const string& word) const {
-        //(word starts with '=' AND is a stop word) OR is a stop word
-        return (word[0] == '-' && stop_words_.count(word.substr(1)) > 0) 
+        //(minus word AND a stop word) OR is a stop word
+        return (word[0] == '-' && stop_words_.count(word.substr(1)) > 0)
         || stop_words_.count(word) > 0;
     }
     
-    //ignores stop words, even with a '-' suffix
-    vector<string> SplitIntoWordsNoStop(const string& text) const {
-        vector<string> words;
-        for (const string& word :  SplitIntoWords(text)) {
+    //ignores stop words
+    map<string, int> SplitIntoWordsNoStop(const string& text) const {
+        map<string, int> words;
+        for (const string& word : SplitIntoWords(text)) {
             if (!IsStopWord(word)) {
-                words.push_back(word);
+                ++words[word];
             }
         }
         return words;
     }
-
-    set<string> ParseQuery(const string& text, set<string>& minusWords) const {
-        set<string> query_words;
-        for (const string& word : SplitIntoWordsNoStop(text)) {
+    
+    vector<QueryWord> ParseQuery(const string& text) const {
+        vector<QueryWord> query_words;
+        for (const auto& [word, _ ] : SplitIntoWordsNoStop(text)) {
+            QueryWord qword(word);
             //found a minus-word
-            if(word[0] == '-') { //cut off the -
-                minusWords.insert(word.substr(1));
-            } else {
-                query_words.insert(word);
+            if(qword.str[0] == '-') {
+                qword.str.erase(0,1);
+                qword.is_minus = true;
             }
+            query_words.push_back(word);
         }
         return query_words;
     }
   
-    vector<Document> FindAllDocumentsNoMinus(const set<string>& query_words, const set<string>& minus_words) const {
-        set<int> minus_list = get_minus_docs(minus_words);
+    vector<Document> FindAllDocumentsNoMinus(const vector<QueryWord>& query_words) const {
+        set<int> minus_list = GetMinusDocs(query_words);
         map<int, double> id_relevance;
         //process each query word in turn
-        for(const auto& word : query_words) {
-            if(word_index_.count(word) != 0) {
-                double IDF = get_idf(word); //once per word
-                for(const auto& [docId, count] : word_index_.at(word)) {
-                    //don't calculate for minus-list docs
-                    if(minus_list.count(docId) == 0) {
-                        double TF = get_tf(docId, count);
-                        id_relevance[docId] += IDF*TF;
+        for(const auto& qword : query_words) {
+            if(word_index_.count(qword.str) != 0) {
+                double inverse_doc_freq = GetIdf(qword.str); //once per word
+                for(const auto& [docId, term_freq] : word_index_.at(qword.str)) {
+                    if(minus_list.count(docId) == 0) {//skip docs with -words
+                        id_relevance[docId] += inverse_doc_freq*term_freq;
                     }
                 }
             }
